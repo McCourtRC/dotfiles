067482710477a09c180ae6baf5eea069f58f5ce0
Corey McCourt <mccourtrc@gmail.com>
2022-06-15 11:53:40 -0700

    fix(nvim): setup nvim tree

--- nvim/init.lua
+++ nvim/init.lua
@@ -74,6 +74,9 @@ require('packer').startup(function(use)
   -- File Tree
   use { 'kyazdani42/nvim-tree.lua',
     requires = 'kyazdani42/nvim-web-devicons',
+    config = function()
+      require('nvim-tree').setup()
+    end
   }
 
   -- Telescope
