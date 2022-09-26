353f4587fa98df1d6b629809f27d867725e6a110
Corey McCourt <mccourtrc@gmail.com>
2022-09-26 10:43:19 -0700

    feat: grep string, cleanup textobjects

---
+++ nvim-npm-dependencies.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# I need to reinstall the npm packages
+# when switching to a new node version
+
+npm install --location=global typescript typescript-language-server
+npm install --location=global vscode-langservers-extracted
+npm install --location=global vscode-langservers-extracted
+npm install --location=global @tailwindcss/language-server
+npm install --location=global svelte-language-server
+npm install --location=global graphql-language-service-cli
+npm install --location=global vscode-langservers-extracted
+npm install --location=global yaml-language-server
+npm install --location=global vscode-langservers-extracted
+npm install --location=global @prisma/language-server
+npm install --location=global prettier eslint
--- nvim/after/queries/textobjects.scm
+++
@@ -1,3 +0,0 @@
-(variable_declaration) @variable.outer
-(lexical_declaration) @variable.outer
-(variable_declarator value: (_) @variable.inner)
