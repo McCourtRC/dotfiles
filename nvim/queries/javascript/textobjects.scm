74ce0df83d78ed71a78eba8e2dde9d8dc2324fd0
Corey McCourt <cmccourt@tubi.tv>
2022-12-13 10:27:36 -0800

    feat: key value pair and array for js

--- nvim/queries/javascript/textobjects.scm
+++ nvim/queries/javascript/textobjects.scm
@@ -1,3 +1,12 @@
 (lexical_declaration) @variable.outer
 (variable_declaration) @variable.outer
 (variable_declarator value: (_) @variable.inner)
+
+(object 
+  (pair
+    key: (_) @key
+    value: (_) @value
+  ) @pair
+)
+
+(array (_) @item.inner)
