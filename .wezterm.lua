32db8d8eaa4921fef665b86b60cb6d41b6e92035
Corey McCourt <cmccourt@tubi.tv>
2024-02-13 12:12:54 -0800

    feat: wezterm keymaps

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,6 +10,25 @@ config.font = wezterm.font("CommitMono")
 config.font_size = 16
 config.line_height = 1.2
 
+config.keys = {
+  { key = "s", mods = "ALT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+  { key = "v", mods = "ALT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+
+  { key = "h", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Left") },
+  { key = "j", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Down") },
+  { key = "k", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Up") },
+  { key = "l", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Right") },
+
+  { key = "r", mods = "ALT", action = wezterm.action.RotatePanes("Clockwise") },
+  { key = "R", mods = "ALT", action = wezterm.action.RotatePanes("CounterClockwise") },
+
+  { key = "y", mods = "ALT", action = wezterm.action.ActivateCopyMode },
+  { key = " ", mods = "ALT", action = wezterm.action.QuickSelect },
+
+  { key = ",", mods = "CMD", action = wezterm.action.MoveTabRelative(-1) },
+  { key = ".", mods = "CMD", action = wezterm.action.MoveTabRelative(1) },
+}
+
 config.quick_select_patterns = {
   -- "foo.*"
 }
